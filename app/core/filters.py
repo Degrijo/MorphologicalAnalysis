508e37f53b81d956128d4710ef6e1740fd2681b3
Degrijo <degrijoyarik@gmail.com>
2020-12-21 05:06:39 +0300

    second  lab finish

--- app/core/filters.py
+++ app/core/filters.py
@@ -1,5 +1,6 @@
 from app.core.models import Document
-from app.core.algorithms import extract_keys, get_key_words
+from app.core.algorithms import extract_keys, key_words_without_stopwords, my_case, frequency_word_case, \
+    short_word_case
 
 
 MANDATORY_CHAR = r'&'
@@ -14,8 +15,14 @@ def get_closest_docs(text):
     docs = Document.objects.values('text')
     agg_docs = []
     for doc in docs:
-        agg_text = get_key_words(doc['text'])
+        agg_text = key_words_without_stopwords(doc['text'])
         if not (mandatory - agg_text) and not (unnecessary & agg_text):
             doc['hit_counter'] = len(agg_text & optional) + len(agg_text & mandatory)
             agg_docs.append(doc)
     return sorted(agg_docs, key=lambda obj: obj['hit_counter'], reverse=True)
+
+
+def define_languages(text):
+    return [('Frequency word case', frequency_word_case(text)),
+            ('Short word case', short_word_case(text)),
+            ('My case', my_case(text))]
